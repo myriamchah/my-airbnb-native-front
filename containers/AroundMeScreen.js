c9320763f556c9fefed5bc93b98eeb9028e08f44
myriam chahlafi <myriam.chah@gmail.com>
2023-11-28 14:52:58 +0700

    Allow user to see rooms around him or all

--- containers/AroundMeScreen.js
+++ containers/AroundMeScreen.js
@@ -1,10 +1,41 @@
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 import { useState, useEffect } from "react";
 import { SafeAreaView, StyleSheet, ActivityIndicator } from "react-native";
 import axios from "axios";
 
 export default function AroundMeScreen({ navigation }) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState([]);
+  const [userCoords, setUserCoords] = useState([48.8564449, 2.4002913]);
+
+  useEffect(() => {
+    const askPermissionAndFetchData = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      let data;
+      if (status === "granted") {
+        const { coords } = await Location.getCurrentPositionAsync();
+
+        const latitude = coords.latitude;
+        const longitude = coords.longitude;
+
+        setUserCoords([latitude, longitude]);
+
+        ({ data } = await axios.get(
+          `https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/rooms/around?latitude=${latitude}&longitude=${longitude}`
+        ));
+      } else {
+        ({ data } = await axios.get(
+          "https://lereacteur-bootcamp-api.herokuapp.com/api/airbnb/rooms"
+        ));
+      }
+
+      setRooms(data);
+      setIsLoading(false);
+    };
+
+    askPermissionAndFetchData();
+  }, []);
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -14,13 +45,30 @@ export default function AroundMeScreen({ navigation }) {
         <MapView
           style={{ flex: 1 }}
           initialRegion={{
-            latitude: 48.856614,
-            longitude: 2.3522219,
+            latitude: userCoords[0],
+            longitude: userCoords[1],
             latitudeDelta: 0.2,
             longitudeDelta: 0.2,
           }}
           showsUserLocation={true}
-        />
+        >
+          {rooms.map((room, i) => {
+            return (
+              <Marker
+                onPress={() => {
+                  navigation.navigate("Room", {
+                    id: room._id,
+                  });
+                }}
+                key={i}
+                coordinate={{
+                  latitude: room.location[1],
+                  longitude: room.location[0],
+                }}
+              />
+            );
+          })}
+        </MapView>
       )}
     </SafeAreaView>
   );
